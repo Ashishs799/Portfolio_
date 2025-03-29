83b7b36f78f5368c1838fc39e1e5b99c7bd2134d
Ashishs799 <np03cs4s220007@heraldcollege.edu.np>
2025-03-29 22:31:12 +0545

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -18,21 +18,6 @@ function App() {
   return (
     <>
       <Layout />
-      {/* <div>
-        <div class="relative w-full">
-          <div class="h-screen bg-red-500 text-white flex items-center justify-center text-3xl font-bold ">
-            Section 1
-          </div>
-
-          <div class="h-screen bg-blue-500 text-white flex items-center justify-center text-3xl font-bold ">
-            Section 2
-          </div>
-
-          <div class="h-screen bg-green-500 text-white flex items-center justify-center text-3xl font-bold sticky bottom-0 -z-10">
-            Section 3
-          </div>
-        </div>
-      </div> */}
 
       {/* {showIntro ? (
         <Loader onComplete={() => setShowIntro(false)} />
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ecommerce from "../assets/images/pahiranlptp.png";
 import taskcascade from "../assets/images/taskcascade.png";
+import { motion } from "framer-motion";
 import tictactoe from "../assets/images/tictactoebg.jpg";
-import taskmanager from "../assets/images/taskmanage.jpg";
+import taskmanager from "../assets/images/taskmanagerapp.jpg";
 import eatwell from "../assets/images/Eatwell.png";
 import { MdArrowOutward } from "react-icons/md";
 
@@ -15,37 +16,42 @@ const Portfolio = () => {
       <h1 className="text-[10px] tracking-[4px] 2xl:text-2xl font-bold 2xl:tracking-[12px] uppercase text-pink-600 mb-8">
         Work
       </h1>
-      <div
-        className=" w-full flex flex-col 2xl:gap-12 gap-y-4
-       "
-      >
-        <div className="flex flex-col 2xl:flex-row 2xl:gap-12 gap-y-4">
-          <div className="rounded-lg overflow-hidden w-full relative group ">
-            {/* <div className="w-full h-full bg-[#a3004ca1] z-10 absolute bottom-0 left-0 opacity-0 group-hover:opacity-100 flex justify-center items-center transition delay-75 duration-200 ease-in">
-              <div className="flex gap-x-4 ">
-                <span className=" py-3 px-6 bg-white rounded-4xl block">
-                  Taskcascade.com
-                </span>
-                <span className=" py-3 px-4 flex justify-center items-center bg-white rounded-4xl ">
-                  <MdArrowOutward />
-                </span>
-              </div>
-            </div> */}
+      <div className="w-full flex flex-col 2xl:gap-12 gap-y-4">
+        <motion.div className="flex flex-col 2xl:flex-row 2xl:gap-12 gap-y-4">
+          <motion.div
+            className="rounded-lg overflow-hidden w-full relative group"
+            initial={{ scale: 0 }}
+            whileInView={{ scale: 1 }}
+            viewport={{ once: true }}
+            transition={{ duration: 1, delay: 0.4 }}
+          >
             <img
               src={taskcascade}
               alt="Image 1"
-              className="w-full h-full absolute top-0 left-0  "
+              className="w-full h-full absolute top-0 left-0"
             />
-          </div>
-          <div className=" 2xl:w-[900px] rounded-lg overflow-hidden 2xl:h-[400px] 2xl:self-end">
+          </motion.div>
+          <motion.div
+            className="2xl:w-[900px] rounded-lg overflow-hidden 2xl:h-[400px] 2xl:self-end"
+            initial={{ scale: 0 }}
+            whileInView={{ scale: 1 }}
+            viewport={{ once: true }}
+            transition={{ duration: 1, delay: 0.4 }}
+          >
             <img
               src={tictactoe}
               alt="Image 2"
               className="w-full h-full object-cover"
             />
-          </div>
-        </div>
-        <div className="2xl:px-40 2xl:py-16">
+          </motion.div>
+        </motion.div>
+        <motion.div
+          initial={{ scale: 0 }}
+          whileInView={{ scale: 1 }}
+          viewport={{ once: true }}
+          transition={{ duration: 1, delay: 0.4 }}
+          className="2xl:px-40 2xl:py-16"
+        >
           <div className="rounded-lg overflow-hidden">
             <img
               src={ecommerce}
@@ -53,23 +59,35 @@ const Portfolio = () => {
               className="w-full h-full object-cover"
             />
           </div>
-        </div>
-        <div className="flex flex-col 2xl:flex-row 2xl:gap-12 gap-y-4">
-          <div className=" 2xl:w-[900px] rounded-lg overflow-hidden">
+        </motion.div>
+        <motion.div className="flex flex-col 2xl:flex-row 2xl:gap-12 gap-y-4">
+          <motion.div
+            className="2xl:w-[900px] rounded-lg overflow-hidden"
+            initial={{ scale: 0 }}
+            whileInView={{ scale: 1 }}
+            viewport={{ once: true }}
+            transition={{ duration: 1, delay: 0.4 }}
+          >
             <img
               src={taskmanager}
               alt="Image 5"
               className="2xl:w-fit h-full object-cover"
             />
-          </div>
-          <div className="rounded-lg overflow-hidden">
+          </motion.div>
+          <motion.div
+            className="rounded-lg overflow-hidden"
+            initial={{ scale: 0 }}
+            whileInView={{ scale: 1 }}
+            viewport={{ once: true }}
+            transition={{ duration: 1, delay: 0.8 }}
+          >
             <img
               src={eatwell}
               alt="Image 4"
               className="w-full h-full object-cover"
             />
-          </div>
-        </div>
+          </motion.div>
+        </motion.div>
       </div>
     </div>
   );
